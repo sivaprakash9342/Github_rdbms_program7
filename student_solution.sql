10b2075a0e2c47f1d9fdb314b5327f7a843ea76b
sivaprakash9342 <sivaprakash934278@gmail.com>
2026-08-27 10:28:55 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,16 @@
+USE CollegeDB;
+CREATE TABLE Marksheet(
+RollNo INT,
+NAME VARCHAR(20),
+Departmant VARCHAR(20),
+Marks INT 
+);
+INSERT INTO Marksheet VALUES
+(1,"Arun","CSE",85),
+(2,"Divya","IT",78),
+(3,"Karthik","CSE",92),
+(4,"Nisha","ECE",67),
+(5,"Rahul","IT",88);
+SELECT*FROM Marksheet
+WHERE Marks>80 ORDER BY Marks DESC;
 
